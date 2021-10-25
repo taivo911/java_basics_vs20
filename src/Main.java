29a1f21c2564894e98b1ae7083412225d9e022b8
taivonurk <taivo.nurk@khk.ee>
2021-10-25 11:36:36 +0300

    Week 5 - ex 88

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import week5.Student;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student pekka = new Student("Pekka Mikkola", "013141590");
+        System.out.println("name: " + pekka.getName());
+        System.out.println("studentnumber: " + pekka.getStudentNumber());
+        System.out.println(pekka);
+    }
+}
