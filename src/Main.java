8eee95a2aa59cc504894da9e4458fb1cd2398a6f
taivonurk <taivo.nurk@khk.ee>
2021-10-25 11:42:35 +0300

    Week 5 - ex 89

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
-import week5.Student;
+import week5.Clock;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Student pekka = new Student("Pekka Mikkola", "013141590");
-        System.out.println("name: " + pekka.getName());
-        System.out.println("studentnumber: " + pekka.getStudentNumber());
-        System.out.println(pekka);
+        Clock clock = new Clock(23, 59, 50);
+
+        int i = 0;
+        while( i < 20) {
+            System.out.println( clock );
+            clock.tick();
+            i++;
+        }
     }
 }
